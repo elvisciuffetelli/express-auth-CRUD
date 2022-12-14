29aa6f2f0a371810d0483bd9dffb3f96ce15f670
Elvis Ciuffetelli <ciuffetellielvis@gmail.com>
2022-12-14 11:44:26 +0100

    express auth and CRUD

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+const PORT = process.env.PORT || 8000;
+
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log('Listen on port ' + PORT))
