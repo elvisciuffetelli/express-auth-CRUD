c7284b3ee633d2bad37085dfd702c3b93efd2410
Elvis Ciuffetelli <ciuffetellielvis@gmail.com>
2022-12-14 11:53:27 +0100

    app listen in callback

--- config/db.js
+++ config/db.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose')
 
-const connectDB= async () => {
+const connectDB= async (callback) => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI)
         console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline)
+        callback
     } catch (error) {
         console.log(error)
         process.exit(1)
--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const PORT = process.env.PORT || 8000;
 
-connectDB()
+
 
 const app = express()
+connectDB(app.listen(PORT, () => console.log('Listen on port ' + PORT)))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -17,4 +18,3 @@ app.use('/api/users', require('./routes/userRoutes'))
 
 app.use(errorHandler)
 
-app.listen(PORT, () => console.log('Listen on port ' + PORT))
