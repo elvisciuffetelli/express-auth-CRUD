f1ba3f4ca352cc1031797fe2ff4b92f79ae0c8c6
Elvis Ciuffetelli <ciuffetellielvis@gmail.com>
2022-12-17 13:24:34 +0100

    update schema exercise

--- models/exerciseModel.js
+++ models/exerciseModel.js
@@ -1,26 +1,26 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const exerciseSchema = mongoose.Schema({
+const setSchema = mongoose.Schema({
+  reps: Number,
+  weight: Number,
+});
+
+const exerciseSchema = mongoose.Schema(
+  {
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'User'
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
     },
     name: {
-        type: String,
-        required: [true, 'Please add a name value']
+      type: String,
+      required: [true, "Please add a name value"],
     },
-    sets: {
-        type: Number
-    },
-    reps: {
-        type: Number
-    },
-    weight: {
-        type: Number
-    }
-}, {
-    timestamps: true
-})
+    sets: [setSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model('Exercise', exerciseSchema)
+module.exports = mongoose.model("Exercise", exerciseSchema);
